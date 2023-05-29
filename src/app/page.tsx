2a5d00ed3e7278a3fc554f60ea31c5c50d283850
Viktoriia <v.stepnova@qlab.studio>
2023-05-29 12:20:34 +0300

    add main banner styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Banner from '@/components/HomeComponents/Banner';
+
 export default function Home() {
-  return <main>APP</main>
+  return (
+    <main>
+      <Banner />
+      {/* <section>Features</section>
+      <section>About us</section>
+      <section>Our works</section>
+      <section>Proccess</section>
+      <section>Testimonials</section>
+      <section>Companies</section>
+      <section>Articles</section> */}
+    </main>
+  )
 }
---
+++ src/components/HomeComponents/Banner/index.tsx
@@ -0,0 +1,22 @@
+import style from './style.module.sass'
+
+const Banner = () => {
+  return (
+    <section className={style.banner}>
+      <div className={style.shape1} />
+      <div className={style.shape2} />
+      <h1>
+        Let’s create <br /> something great <br /> together.
+      </h1>
+      <button className={style.button}>
+        <div className={style.circle} />
+        BOOK A FREE CALL <div className={style.arrowRight} />
+      </button>
+      <div className={style.leftHand} />
+      <div className={style.rightHand} />
+      <div className={style.knit} />
+    </section>
+  )
+}
+
+export default Banner
